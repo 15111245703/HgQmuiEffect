96fb91b4a98f921534da6ca5b49296f19569617b
Ricardo <15111245703@163.com>
2017-12-25 11:22:22 +0800

    Initial commit

---
+++ HgQmui/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HgQmui
+//
+//  Created by RicardoM Lu on 2017/12/6.
+//  Copyright © 2017年 RicardoM Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
